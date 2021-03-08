f7d7ea650d21736c1dc0f333212abcc3bf1e6e3b
iMatthewCM <imatthewcm@gmail.com>
2021-03-08 16:25:09 -0600

    Updated with description and possible return values

--- ExtensionAttributes/bootstrapTokenAllowedForAuthentication.sh
+++ ExtensionAttributes/bootstrapTokenAllowedForAuthentication.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+#############################################
+#	 Created by iMatthewCM on 03/08/2021	#
+#################################################################################################
+# This script is not an official product of Jamf Software LLC. As such, it is provided without  #
+# warranty or support. By using this script, you agree that Jamf Software LLC is under no 		#
+# obligation to support, debug, or otherwise maintain this script. Licensed under MIT.			#
+#																								#
+# NAME: bootstrapTokenAllowedForAuthentication.sh												#
+# DESCRIPTION: This script is to be used in an Extension Attribute in Jamf Pro. A use case for  #
+# implementation is a kernel extension approval workflow, where this value would need to be     #
+# "supported" for M1 Macs																		#
+#																								#
+# POSSIBLE RETURN VALUES:																		#
+# not supported																					#
+# allowed																						#
+# disallowed																					#
+#																								#
+# ("not supported" for Intel Macs, "allowed" if we're good to go, "disallowed" if not			#
+#################################################################################################
+
 #Get the value for the BootstrapTokenAllowedForAuthentication key
 output=$(/usr/libexec/mdmclient QuerySecurityInfo | awk -F '[=]' '/BootstrapTokenAllowedForAuthentication/ {print $NF}' | tr -d '";')
 
